d7ecb7b92efe7d13b336f72661383b04e54f783f
Swaroop Kumar Dasari <dasari.kumar@mountblue.tech>
2020-12-10 06:52:02 +0000

    Update ConnectToDatabase.java

--- ContactUsWithJspAndServlets/src/io/mountblue/learn/dbconnect/ConnectToDatabase.java
+++ ContactUsWithJspAndServlets/src/io/mountblue/learn/dbconnect/ConnectToDatabase.java
@@ -8,23 +8,15 @@ public class ConnectToDatabase {
 	private static final String url = "jdbc:postgresql://localhost:5432/Requests";
 	private static final String name = "postgres";
 	private static final String dbPassword = "somu9042";
-	
+
 	public Connection dbConnect() {
-		loadPostgresDriver();
-		Connection con=null;
+		Connection con = null;
 		try {
+			Class.forName("org.postgresql.Driver");
 			con = DriverManager.getConnection(url, name, dbPassword);
-		} catch (SQLException e) {
+		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 		return con;
 	}
-	private void loadPostgresDriver() {
-		try {
-			Class.forName("org.postgresql.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-	
 }
